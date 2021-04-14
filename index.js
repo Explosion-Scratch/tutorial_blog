02ad9d16988d1d6bd45e40f9d63bd75ecce5e347
--Explosion-- <explosion.scratch@gmail.com>
2021-04-14 12:23:47 +0000

    More stuff

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const fs = require("fs");
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.use(express.static('public'))
 app.engine('hbs', exphbs({
     defaultLayout: 'main',
@@ -16,7 +17,15 @@ hbs.handlebars.registerHelper('markdown', function(markdown) {
   var converter = new showdown.Converter();
 	return converter.makeHtml(markdown);
 })
-
+hbs.handlebars.registerHelper('clipText', function(text) {
+	text = text.slice(0, 150);
+	text += text.length === 150 ? "..." : "";
+	return text;
+})
+hbs.handlebars.registerHelper('firstSection', function(text) {
+	text = text.replace(/^\n+/, "").split(/\n/)[0];
+	return text;
+})
 hbs.handlebars.registerHelper('readingTime', function(text) {
   const readTime = require('reading-time');
 	return readTime(text).text
@@ -28,13 +37,27 @@ app.get('/', (req, res) => {
   res.render('index')
 });
 
+app.get("/meta.json", (req, res) => {
+	res.status(200).json({author_name: "--Explosion--", author_url: `https://scratch.mit.edu/users/--Explosion--`, provider_name: "JS Tutorial Blog", provider_url: "https://blog.explosionscratc.repl.co"})
+})
 app.get("/post/:id", (req, res) => {
-	var post = {};
-	post.text = fs.readFileSync(`${__dirname}/posts/${req.params.id}.md`, "utf-8");
-	post.title = post.text.match(/^\#(.+)$/m)[1];
-	post.text = post.text.replace(/^\#.+$/m, "").replace(/^\n/, "")
-	console.log(post)
-	res.render("post", {post})
+	try {
+		var post = {};
+		post.text = fs.readFileSync(`${__dirname}/posts/${req.params.id}.md`, "utf-8");
+		post.title = post.text.match(/^\#(.+)$/m)[1];
+		post.text = post.text.replace(/^\#.+$/m, "").replace(/^\n/, "");
+		post.id = req.params.id;
+		res.render("post", {post})
+	} catch(e){
+		res.render("error", {error: "Sorry! That post doesn't exist!"})
+	}
+})
+app.get("/new", (req, res) => {
+	res.render("new")
+})
+app.post("/new_post", (req, res) => {
+	console.log(req.body);
+	res.redirect("/")
 })
 app.listen(3000, () => {
   console.log('server started');
